316341ec437d74616f408cb7697472490ee286b2
Cliff Moon <cliff@moonpolysoft.com>
2010-01-22 16:23:39 -0800

    added breaking test for dict encoding round trip.  not working in R13B03.
    Also: what's with the anemic test coverage?

--- test/bert_test.erl
+++ test/bert_test.erl
@@ -9,6 +9,11 @@ encode_list_nesting_test() ->
 encode_tuple_nesting_test() ->
   Bert = term_to_binary({foo, {bert, true}}),
   Bert = encode({foo, true}).
+  
+round_trip_dict_test() ->
+  D = dict:store("my", balls, dict:store("number", 1234, dict:store("suckit", [], dict:new()))),
+  Bin = encode(D),
+  ?assertEqual(D, decode(Bin)).
 
 %% decode
 
