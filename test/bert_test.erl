d5981aea8c1ccddf4fe2617b9e437a1cdf2e7e1f
b <b@b3k.us>
2010-01-25 23:25:02 -0800

    Fixes for dict coding and added a test from Cliff

--- test/bert_test.erl
+++ test/bert_test.erl
@@ -1,22 +1,27 @@
 -include_lib("eunit/include/eunit.hrl").
-
+ 
 %% encode
-
+ 
 encode_list_nesting_test() ->
   Bert = term_to_binary([foo, {bert, true}]),
   Bert = encode([foo, true]).
-
+ 
 encode_tuple_nesting_test() ->
   Bert = term_to_binary({foo, {bert, true}}),
   Bert = encode({foo, true}).
-
+  
+round_trip_dict_test() ->
+  D = dict:store("my", balls, dict:store("number", 1234, dict:store("suckit", [], dict:new()))),
+  Bin = encode(D),
+  ?assertEqual(D, decode(Bin)).
+ 
 %% decode
-
+ 
 decode_list_nesting_test() ->
   Bert = term_to_binary([foo, {bert, true}]),
   Term = [foo, true],
   Term = decode(Bert).
-
+ 
 decode_tuple_nesting_test() ->
   Bert = term_to_binary({foo, {bert, true}}),
   Term = {foo, true},
