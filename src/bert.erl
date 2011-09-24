4b319034de5d5694f56a61baa2a44b7280adb4b3
Aleksey Zhukov <drdaeman@drdaeman.pp.ru>
2011-09-24 23:39:55 +0400

    Ran bert.erl through Tidier (http://tidier.softlab.ntua.gr:20000/)

--- src/bert.erl
+++ src/bert.erl
@@ -11,57 +11,51 @@
 %% Public API
 
 -spec encode(term()) -> binary().
-
 encode(Term) ->
-  term_to_binary(encode_term(Term)).
+    term_to_binary(encode_term(Term)).
 
 -spec decode(binary()) -> term().
-
 decode(Bin) ->
-  decode_term(binary_to_term(Bin)).
+    decode_term(binary_to_term(Bin)).
 
 %%---------------------------------------------------------------------------
 %% Encode
 
 -spec encode_term(term()) -> term().
-
 encode_term(Term) ->
-  case Term of
-    [] -> {bert, nil};
-    true -> {bert, true};
-    false -> {bert, false};
-    Dict when is_tuple(Term) andalso element(1, Term) =:= dict ->
-      {bert, dict, lists:map(fun({K, V}) ->
-        {encode_term(K), encode_term(V)} end, dict:to_list(Dict))};
-    List when is_list(Term) ->
-      lists:map((fun encode_term/1), List);
-    Tuple when is_tuple(Term) ->
-      TList = tuple_to_list(Tuple),
-      TList2 = lists:map((fun encode_term/1), TList),
-      list_to_tuple(TList2);
-    _Else -> Term
-  end.
+    case Term of
+        [] -> {bert, nil};
+        true -> {bert, true};
+        false -> {bert, false};
+        Dict when is_tuple(Term) andalso element(1, Term) =:= dict ->
+            {bert, dict, [{encode_term(K), encode_term(V)}
+                          || {K, V} <- dict:to_list(Dict)]};
+        List when is_list(Term) ->
+            [encode_term(V2) || V2 <- List];
+        Tuple when is_tuple(Term) ->
+            TList = tuple_to_list(Tuple),
+            TList2 = [encode_term(V3) || V3 <- TList],
+            list_to_tuple(TList2);
+        _Else -> Term
+    end.
 
 %%---------------------------------------------------------------------------
 %% Decode
 
 -spec decode_term(term()) -> term().
-
 decode_term(Term) ->
-  case Term of
-    {bert, nil} -> [];
-    {bert, true} -> true;
-    {bert, false} -> false;
-    {bert, dict, Dict} ->
-      L = lists:map(fun decode_term/1, Dict),
-      dict:from_list(L);
-    {bert, Other} ->
-      {bert, Other};
-    List when is_list(Term) ->
-      lists:map((fun decode_term/1), List);
-    Tuple when is_tuple(Term) ->
-      TList = tuple_to_list(Tuple),
-      TList2 = lists:map((fun decode_term/1), TList),
-      list_to_tuple(TList2);
-    _Else -> Term
-  end.
+    case Term of
+        {bert, nil} -> [];
+        {bert, true} -> true;
+        {bert, false} -> false;
+        {bert, dict, Dict} ->
+            L = [decode_term(V) || V <- Dict], dict:from_list(L);
+        {bert, _Other} = Bert5 -> Bert5;
+        List when is_list(Term) ->
+            [decode_term(V1) || V1 <- List];
+        Tuple when is_tuple(Term) ->
+            TList = tuple_to_list(Tuple),
+            TList2 = [decode_term(V2) || V2 <- TList],
+            list_to_tuple(TList2);
+        _Else -> Term
+    end.
